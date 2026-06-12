be0be99454036fbb12d1561634ba7d582f54b47f
TharinduPasanMeewala <saragroupit2@gmail.com>
2026-06-12 11:26:18 +0530

    Add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import DashboardPage from './pages/Dashboard.jsx'
+import InvoicesPage from './pages/Invoices.jsx'
+import EarningsPage from './pages/Earnings.jsx'
+import TimeEntriesPage from './pages/TimeEntries.jsx'
 import ERPSyncLogPage from './pages/ERPSyncLog.jsx'
 import ShipmentPage from './pages/Shipment.jsx'
 import OrderPage from './pages/Order.jsx'
 import StockMovementPage from './pages/StockMovement.jsx'
 import InventoryPage from './pages/Inventory.jsx'
-import LotSerialPage from './pages/LotSerial.jsx'
-import MobileReceivingPutawayPage from './pages/MobileReceivingPutaway.jsx'
-import SystemSettingsConfigurationPage from './pages/SystemSettingsConfiguration.jsx'
-import TenantManagementPage from './pages/TenantManagement.jsx'
 
 export default function App() {
   return (
@@ -17,15 +16,14 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
+          <Route path="/invoices" element={<InvoicesPage />} />
+          <Route path="/earnings" element={<EarningsPage />} />
+          <Route path="/timeentries" element={<TimeEntriesPage />} />
           <Route path="/erpsynclog" element={<ERPSyncLogPage />} />
           <Route path="/shipment" element={<ShipmentPage />} />
           <Route path="/order" element={<OrderPage />} />
           <Route path="/stockmovement" element={<StockMovementPage />} />
           <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/lotserial" element={<LotSerialPage />} />
-          <Route path="/mobile/receiving" element={<MobileReceivingPutawayPage />} />
-          <Route path="/admin/settings" element={<SystemSettingsConfigurationPage />} />
-          <Route path="/admin/tenants" element={<TenantManagementPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
