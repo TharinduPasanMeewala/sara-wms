0534f274a731fbae1b6805adb6094161cd5ef8ad
TharinduPasanMeewala <saragroupit2@gmail.com>
2026-06-12 10:00:16 +0530

    Add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MobileReceiving from './pages/MobileReceiving';
 import AdminSettings from './pages/AdminSettings';
-import TenantManagement from './pages/TenantManagement';
-import RoleConfiguration from './pages/RoleConfiguration';
-import UserManagement from './pages/UserManagement';
+import AdminTenants from './pages/AdminTenants';
+import AdminRoles from './pages/AdminRoles';
+import AdminUsers from './pages/AdminUsers';
 import StockMovementReport from './pages/StockMovementReport';
 import InventoryAgingReport from './pages/InventoryAgingReport';
 import ERPIntegrationMonitor from './pages/ERPIntegrationMonitor';
 import ShipmentDetail from './pages/ShipmentDetail';
 import ShipmentManagement from './pages/ShipmentManagement';
-import Dashboard from './pages/Dashboard';
 import Layout from './components/Layout';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path='/' element={<Dashboard />} />
           <Route path='/mobile/receiving' element={<MobileReceiving />} />
           <Route path='/admin/settings' element={<AdminSettings />} />
-          <Route path='/admin/tenants' element={<TenantManagement />} />
-          <Route path='/admin/roles' element={<RoleConfiguration />} />
-          <Route path='/admin/users' element={<UserManagement />} />
+          <Route path='/admin/tenants' element={<AdminTenants />} />
+          <Route path='/admin/roles' element={<AdminRoles />} />
+          <Route path='/admin/users' element={<AdminUsers />} />
           <Route path='/reports/stock-movements' element={<StockMovementReport />} />
           <Route path='/reports/inventory-aging' element={<InventoryAgingReport />} />
           <Route path='/integrations/erp' element={<ERPIntegrationMonitor />} />
@@ -33,6 +30,6 @@ const App = () => {
       </Layout>
     </Router>
   );
-};
+}
 
 export default App;
